436647d68708b4b8554bc5b33dc9d3b7ae6be682
Krish Khatiwada <krishkhatiwada125@gmail.com>
2024-06-28 17:49:11 +0200

    doneeeee

--- script1.js
+++ script1.js
@@ -4,16 +4,29 @@ let h5 = document.querySelector("h5")
 let add = document.querySelector(".add")
 let remove = document.querySelector(".remove")
 
+let click = 1
 add.addEventListener("click", function(){
-    h1.innerHTML = "Will Byers"
+    if(click === 1){
+    h1.innerHTML = "CoolBiralo"
     h5.innerHTML = "2 Mutual Friends"
     add.innerHTML = "Confirm"
     remove.innerHTML = "Delete"
+    img.setAttribute("src", "https://images.unsplash.com/photo-1585699777545-355976789272?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTB8fHxlbnwwfHx8fHw%3D")
+    click++
+    }
+    else {
+    h5.innerHTML = "You are now friends"
+    add.innerHTML = ""
+    remove.innerHTML = ""
+    add.style.visibility = "hidden"
+    remove.style.visibility = "hidden"
+    }
 })
 
 remove.addEventListener("click", function(){
-    h1.innerHTML = "Lucas Warner"
-    h5.innerHTML = "Friend"
-    add.innerHTML = "Add Friend"
-    remove.innerHTML = "Remove"
+    h5.innerHTML = "you're not interested"
+    add.innerHTML = ""
+    remove.innerHTML = ""
+    add.style.visibility = "hidden"
+    remove.style.visibility = "hidden"
 })
